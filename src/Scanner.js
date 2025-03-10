cfc804c2aacf9e1845917da0627ad3cc02a2c632
unknown <Administrator@xs-hdzhong.xinsha.com>
2025-03-10 16:08:32 +0800

    migrated to html5-qrcode

--- src/Scanner.js
+++ src/Scanner.js
@@ -1,17 +1,29 @@
-import { useState } from "react";
-import { useZxing } from "react-zxing";
+import { useState, useEffect } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
 
 export const Scanner = () => {
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner("reader", {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    });
+
+    const success = (result) => {
+      scanner.clear();
+    };
+    const error = (err) => {
+      console.warn(err);
+    };
+    scanner.render(success, error);
+  }, []);
   const [result, setResult] = useState("");
-  const { ref } = useZxing({
-    onDecodeResult(result) {
-      setResult(result.getText());
-    },
-  });
 
   return (
     <>
-      <video ref={ref} />
+      <div id="reader"></div>
       <p>
         <span>Last result:</span>
         <span>{result}</span>
